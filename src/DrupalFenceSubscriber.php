83307053128225e6823ae23750045afea0f1cb84
FleetAdmiralButter <quahmingsheng@me.com>
2021-02-14 16:32:04 +1100

    Add flood control logic

--- src/DrupalFenceSubscriber.php
+++ src/DrupalFenceSubscriber.php
@@ -12,10 +12,18 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
  */
 class DrupalFenceSubscriber implements EventSubscriberInterface {
 
-    public function DrupalFenceLoad(GetResponseEvent $event) {
-        // do stuff
+    public function DrupalFenceCheckPath(GetResponseEvent $event) {
         $path = \Drupal::request()->getRequestUri();
-        drupal_set_message('DrupalFence event fired at ' . $path);
+        $isAllowed = _drupal_fence_is_allowed(\Drupal::request()->getClientIp());
+        $isExploitPath = $this->_drupal_fence_check_path($path);
+
+        if ($isExploitPath) {
+            $this->_drupal_fence_log_violation();
+        }
+        
+        if (!$isAllowed || $isExploitPath) {
+            throw new \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException();
+        }
     }
 
 
@@ -23,7 +31,26 @@ class DrupalFenceSubscriber implements EventSubscriberInterface {
      * {@inheritdoc}
      */
     static function getSubscribedEvents() {
-        $events[KernelEvents::REQUEST] = array('DrupalFenceLoad', 20);
+        $events[KernelEvents::REQUEST] = array('DrupalFenceCheckPath', 20);
         return $events;
+    }
+
+    private function _drupal_fence_is_allowed($client_identifier) {
+        return \Drupal::flood()->isAllowed('drupal_fence.security_violation', 5, 3600, $client_identifier);
+    }
+
+    private function _drupal_fence_log_violation() {
+        $client_identifier = \Drupal::request()->getClientIp();
+        \Drupal::flood()->register('drupal_fence.security_violation', 3600, $client_identifier);
+    }
+
+    private function _drupal_fence_check_path($path) {
+        $database = \Drupal::service('database');
+        $result = $database->query("SELECT * FROM {drupal_fence_flagged_routes} WHERE exploit_uri LIKE :current_path",
+        [
+            ':current_path' => $path,
+        ])->fetchAll();
+
+        return (count($result) > 0) ? TRUE : FALSE;
     }
 }
