4179da43f7c6878e0b3c046cf8d66ccac8786e99
FleetAdmiralButter <quahmingsheng@me.com>
2021-02-28 15:51:49 +1100

    Fix issue with logging blocked clients

--- src/DrupalFenceSubscriber.php
+++ src/DrupalFenceSubscriber.php
@@ -20,12 +20,11 @@ class DrupalFenceSubscriber implements EventSubscriberInterface {
         $isBlocked = \Drupal::service('drupal_fence.request_checker')->is_blocked_client($client_identifier);
         $isExploitPath = \Drupal::service('drupal_fence.request_checker')->check_path($path);
 
-        // Only log if the client is not yet blocked by flood control to avoid overloading the database.
-        if (!$isBlocked && $isExploitPath) {
-            \Drupal::service('drupal_fence.request_checker')->log_violation($client_identifier);
-        }
-        
-        if ($isBlocked || $isExploitPath) {
+        if ($isExploitPath) {
+            // Only log if the client is not yet blocked by flood control to avoid overloading the database.
+            if (!$isBlocked) {
+                \Drupal::service('drupal_fence.request_checker')->log_violation($client_identifier);
+            }
             throw new \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException();
         }
     }
