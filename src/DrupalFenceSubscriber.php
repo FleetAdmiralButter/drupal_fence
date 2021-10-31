fe29cd4acbea72c1f182875f3710a78d009578c4
FleetAdmiralButter <ming.quah@protonmail.com>
2021-10-31 14:48:16 +1100

    Implement DI for EventSubscriber

--- src/DrupalFenceSubscriber.php
+++ src/DrupalFenceSubscriber.php
@@ -6,39 +6,55 @@ use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
 use Drupal\Core\Site\Settings;
+use Drupal\Core\Config\ConfigFactory;
+use Drupal\Core\PageCache\ResponsePolicy\KillSwitch;
+
 /**
  * Provides a DrupalFenceSubscriber
  */
 class DrupalFenceSubscriber implements EventSubscriberInterface {
 
-    public function DrupalFenceCheckRequest(GetResponseEvent $event) {
+    private $config_factory;
+    private $request;
+    private $drupal_fence_request_checker;
+    private $page_cache_kill_switch;
+    public function __construct(ConfigFactory $config_factory, RequestStack $request, DrupalFenceRequestChecker $drupal_fence_request_checker, KillSwitch $page_cache_kill_switch) {
+        $this->config_factory = $config_factory;
+        $this->request = $request->getCurrentRequest();
+        $this->drupal_fence_request_checker = $drupal_fence_request_checker;
+        $this->page_cache_kill_switch = $page_cache_kill_switch;
+    }
+
+    public function onKernelRequest($event) {
 
         // Don't run if Drupal Fence is disabled or this isn't a master request
-        if ((\Drupal::config('drupal_fence.settings')->get('drupal_fence.enabled') == 0) || !($event->getRequestType() === HttpKernelInterface::MASTER_REQUEST)) {
+        if (($this->config_factory->get('drupal_fence.settings')->get('drupal_fence.enabled') == 0) || !($event->getRequestType() === HttpKernelInterface::MASTER_REQUEST)) {
             return;
         }
 
-        $silent_mode = \Drupal::config('drupal_fence.settings')->get('drupal_fence.silent_mode');
-        $block_response = \Drupal::config('drupal_fence.settings')->get('drupal_fence.block_response');
+        $silent_mode = $this->config_factory->get('drupal_fence.settings')->get('drupal_fence.silent_mode');
+        $block_response = $this->config_factory->get('drupal_fence.settings')->get('drupal_fence.block_response');
 
-        $client_identifier = \Drupal::request()->getClientIp();
-        $path = \Drupal::request()->getRequestUri();
+        $client_identifier = $this->request->getClientIp();
+        $path = $this->request->getRequestUri();
 
-        $is_blocked = \Drupal::service('drupal_fence.request_checker')->is_blocked_client($client_identifier);
-        $is_exploit_path = \Drupal::service('drupal_fence.request_checker')->check_path($path);
+        $is_blocked = $this->drupal_fence_request_checker->is_blocked_client($client_identifier);
+        $is_exploit_path = $this->drupal_fence_request_checker->check_path($path);
 
         // Only log if the client is not already blocked by flood control.
         if ($is_exploit_path) {
             if (!$is_blocked) {
-                \Drupal::service('drupal_fence.request_checker')->log_violation($client_identifier);
+                $this->drupal_fence_request_checker->log_violation($client_identifier);
             }
         }
 
         // Block client if path matches or if they have been blocked by flood control and silent mode is disabled.
         if ($is_exploit_path || $is_blocked) {
             // Do not cache these
-            \Drupal::service('page_cache_kill_switch')->trigger();
+            $this->page_cache_kill_switch->trigger();
             if (!$silent_mode) {
                 if ($block_response === '403') {
                   throw new \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException();
@@ -53,7 +69,7 @@ class DrupalFenceSubscriber implements EventSubscriberInterface {
      * {@inheritdoc}
      */
     static function getSubscribedEvents() {
-        $events[KernelEvents::REQUEST] = array('DrupalFenceCheckRequest', Settings::get('drupal_fence.listener_priority', 1000));
+        $events[KernelEvents::REQUEST] = array('onKernelRequest', Settings::get('drupal_fence.listener_priority', 1000));
         return $events;
     }
 }
